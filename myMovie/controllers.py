9e34a92d4ef6f49296ef8c45f1c994d792d76b7a
hankshz <hankshz@gmail.com>
2017-08-26 00:10:02 -0700

    Add file upload

--- myMovie/controllers.py
+++ myMovie/controllers.py
@@ -1,11 +1,28 @@
+import hashlib
+import os
+
 from flask import render_template
 from flask import abort
+from flask import request
+from werkzeug.utils import secure_filename
+
 
 from myMovie import app
 from myMovie import apiManager
 
 for model in app.config['API_MODELS']:
     apiManager.create_api(model.modelClass, methods=model.modelMethods)
+
+@app.route('/fileUpload', methods=['POST'])
+def fileUpload():
+    if 'file' in request.files:
+        uploadedFile = request.files['file']
+        if uploadedFile.filename != '':
+            filename = secure_filename(uploadedFile.filename)
+            hashname = hashlib.md5(uploadedFile.read()).hexdigest()
+            uploadedFile.save(os.path.join(app.config['UPLOAD_FOLDER'], hashname))
+            return 'hello'
+    abort(409)
 
 # It's a single page app, so as long as it's not sent to /api, just let angular to handle it
 @app.route('/', defaults={'path': ''})
--- myMovie/static/js/controllers.js
+++ myMovie/static/js/controllers.js
@@ -12,31 +12,87 @@ angular.module('myMovieControllers').controller('AboutController', ['$scope', fu
 
 angular.module('myMovieControllers').controller('WatchController', ['$scope', 'Movie', function ($scope, Movie) {
     var moviesQuery = Movie.get({}, function(movies) {
-		$scope.movies = movies.objects;
-	});
+        $scope.movies = movies.objects;
+    });
 }]);
 
 angular.module('myMovieControllers').controller('MovieController', ['$scope', '$routeParams', '$sce', 'Movie', function ($scope, $routeParams, $sce, Movie) {
     var movieQuery = Movie.get({ movieId: $routeParams.movieId}, function(movie) {
-		$scope.movie = movie;
+        $scope.movie = movie;
         $scope.player = {
-				sources: [
-					{src: $sce.trustAsResourceUrl(movie.location), type: "video/mp4"}
-				],
-				theme: "/static/css/videogular.css"
-			};
-	});
+                sources: [
+                    {src: $sce.trustAsResourceUrl(movie.location), type: "video/mp4"}
+                ],
+                theme: "/static/css/videogular.css"
+            };
+    });
 }]);
 
-angular.module('myMovieControllers').controller('CreateController', ['$scope', '$uibModalInstance', function($scope, $uibModalInstance){
-    $scope.createInput = '';
+angular.module('myMovieControllers').controller('CreateController', ['$scope', '$uibModalInstance', 'FileUploader', function($scope, $uibModalInstance, FileUploader){
+    var uploader = $scope.uploader = new FileUploader({
+            url: 'fileUpload'
+        });
+
+        // FILTERS
+
+        // a sync filter
+        uploader.filters.push({
+            name: 'syncFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                console.log('syncFilter');
+                return this.queue.length < 10;
+            }
+        });
+
+        // an async filter
+        uploader.filters.push({
+            name: 'asyncFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options, deferred) {
+                console.log('asyncFilter');
+                setTimeout(deferred.resolve, 1e3);
+            }
+        });
+
+        // CALLBACKS
+
+        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        uploader.onAfterAddingFile = function(fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+        };
+        uploader.onAfterAddingAll = function(addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        uploader.onBeforeUploadItem = function(item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        uploader.onProgressItem = function(fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        uploader.onProgressAll = function(progress) {
+            console.info('onProgressAll', progress);
+        };
+        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        uploader.onErrorItem = function(fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        uploader.onCancelItem = function(fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
 
     $scope.ok = function () {
-        $uibModalInstance.close($scope.createInput);
-    };
-
-    $scope.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
+        $uibModalInstance.close();
     };
 }]);
 
@@ -53,9 +109,8 @@ angular.module('myMovieControllers').controller('DownloadController', ['$scope',
             appendTo: undefined,
         });
 
-        modalInstance.result.then(function (inputs) {
-            //$scope.test = JSON.parse(inputs);
-            $scope.test = inputs;
+        modalInstance.result.then(function () {
+            //Refresh page
         }, function () {
             console.log('Modal dismissed at: ' + new Date());
         });
